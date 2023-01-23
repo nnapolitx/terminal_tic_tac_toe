edb37da5c4c9025ad44b3a8fef06f8cc2b952878
nnapolitx <nnapolitano88@gmail.com>
2023-01-23 10:41:32 -0300

    refactoring player's move validation to own method

--- terminal_tictactoe.rb
+++ terminal_tictactoe.rb
@@ -73,6 +73,10 @@ def current_turn(one, two)
   end
 end
 
+def validate_player_move(current_move, current_player)
+  # check the move to see if valid and pass to player's board
+end
+
 def input_move(current_player, gameboard, player_one, player_two)
   current_board(gameboard)
   puts "#{current_player.name}, please choose an open space."
@@ -81,11 +85,20 @@ def input_move(current_player, gameboard, player_one, player_two)
   if current_player.board.include?(current_move)
     puts 'Space is already taken, please choose an EMPTY space'
     input_move
-  else
+  elsif current_move > 0 && current_move < 10
     current_player.move(current_move)
     gameboard[current_move-1] = current_player.sym
+  else
+    puts "#{current_move} is not a valid move. Please enter a number 1 through 9 to choose an empty space"
   end
   input_move(current_turn(player_one, player_two), gameboard, player_one, player_two)
 end
 
 input_move(current_turn(player_one, player_two), gameboard, player_one, player_two)
+
+# TODO
+# Account for error inputs such as letters or numbers >9||<0
+# Call TIE if gameboard.length>=9
+# Add a check winner module
+# add a Play Again
+# Refactor and namespace to make it easier to call a new game

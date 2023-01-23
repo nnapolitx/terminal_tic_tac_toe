3d47e099667944c6434442a279759bd6b65ae66e
nnapolitx <nnapolitano88@gmail.com>
2023-01-23 11:07:46 -0300

    trying to refactor the input code so that each step is a method

--- terminal_tictactoe.rb
+++ terminal_tictactoe.rb
@@ -73,25 +73,41 @@ def current_turn(one, two)
   end
 end
 
-def validate_player_move(current_move, current_player)
+def validate_player_move(current_move, current_player, gameboard)
   # check the move to see if valid and pass to player's board
+  if current_player.board.include?(current_move)
+    puts 'Space is already taken, please choose an EMPTY space'
+    input_move
+  elsif current_move.positive? && current_move < 10
+    current_player.move(current_move)
+    gameboard[current_move - 1] = current_player.sym
+  else
+    puts "#{current_move} is not a valid move. Please enter a number 1 through 9 to choose an empty space"
+  end
+end
+
+def check_winner(current_player, gameboard, player_one, player_two)
+  # check for win or tie
+  if gameboard.length >= 9
+    puts "#{current_board(gameboard)} There was a tie, please play again"
+  else
+    input_move(current_turn(player_one, player_two), gameboard, player_one, player_two)
+  end
 end
 
 def input_move(current_player, gameboard, player_one, player_two)
   current_board(gameboard)
   puts "#{current_player.name}, please choose an open space."
 
-  current_move = gets.chomp.to_i
-  if current_player.board.include?(current_move)
-    puts 'Space is already taken, please choose an EMPTY space'
-    input_move
-  elsif current_move > 0 && current_move < 10
-    current_player.move(current_move)
-    gameboard[current_move-1] = current_player.sym
+  current_move = gets.chomp
+  if current_move.match(/[a-zA-Z]/)
+    puts "#{current_move} is not valid. Please enter a number 1-10"
+    input_move(current_turn(player_one, player_two), gameboard, player_one, player_two)
   else
-    puts "#{current_move} is not a valid move. Please enter a number 1 through 9 to choose an empty space"
+    current_move = current_move.to_i
+    validate_player_move(current_move, current_player, gameboard)
+    check_winner(current_player, gameboard, player_one, player_two)
   end
-  input_move(current_turn(player_one, player_two), gameboard, player_one, player_two)
 end
 
 input_move(current_turn(player_one, player_two), gameboard, player_one, player_two)
